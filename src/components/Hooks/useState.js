7997f7634aa4652cb6d53e858cd702fbca49a2d5
Rajat <2020kucp1064@iiitkota.ac.in>
2022-06-15 10:03:10 +0530

    all file added

---
+++ src/components/Hooks/useState.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./style.css";
+
+const Usestate = () => {
+  return (
+    <>
+      <div className="center_div">
+        <p>My Data</p>
+        <div class="button2">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          INCR
+        </div>
+        <div class="button2">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          DECR
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Usestate;
